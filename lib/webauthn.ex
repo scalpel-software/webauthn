e9b7ecce46cd0d6819e776788ecf872c341e737e
Thomas Cioppettini <tom@tecrails.com>
2019-07-18 08:15:45 -0400

    Initializaed new repo

---
+++ lib/webauthn.ex
@@ -0,0 +1,18 @@
+defmodule Webauthn do
+  @moduledoc """
+  Documentation for Webauthn.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Webauthn.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/webauthn_test.exs
@@ -0,0 +1,8 @@
+defmodule WebauthnTest do
+  use ExUnit.Case
+  doctest Webauthn
+
+  test "greets the world" do
+    assert Webauthn.hello() == :world
+  end
+end
